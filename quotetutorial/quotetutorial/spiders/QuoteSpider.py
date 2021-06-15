095f3b31cf25f0fd15e331f6bccd6b5257f2423c
Kap-co <wangdaniel0205@gmail.com>
2021-06-15 16:43:23 +0900

    updates

--- quotetutorial/quotetutorial/spiders/QuoteSpider.py
+++ quotetutorial/quotetutorial/spiders/QuoteSpider.py
@@ -7,7 +7,25 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        '''
         # response contains all the source code of the url
-
         title = response.css('title::text').extract() # get title
-        yield {'title': title} # scrapy should use yield instead of return
+        #yield {'title': title} # scrapy should use yield instead of return
+
+        # target element example: <span class="text" itemprop="text"> ... </span>
+        quotes = response.css('span.text::text').extract() 
+        # OR: response.xpath("//span[@class='text']/text()").extract()
+        yield {'title':title, 'quotes': quotes}
+        '''
+        title = response.css('title::text').extract() # get title
+        all_div_quotes = response.css('div.quote')
+
+        for quote in all_div_quotes:
+            quotes = quote.css('span.text::text').extract()
+            authors = quote.css('.author::text').extract()
+            tags = quote.css('.tag::text').extract()
+            yield {
+                'quotes': quotes,
+                'author': authors,
+                'tags': tags
+            }
